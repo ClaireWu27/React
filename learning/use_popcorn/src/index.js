276004f5bfee9362c25ee428d70e8e0993be3b87
Claire Wu <wumeihui0207@gmail.com>
2024-09-17 10:24:02 +1000

    done

--- learning/use_popcorn/src/index.js
+++ learning/use_popcorn/src/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+// import "./StarRating";
+// import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="orange" maxRating={10} onSetRating={setMovieRating} />
+//       <p>this movie is rated {movieRating} </p>
+//     </div>
+//   );
+// }
+root.render(
+  <App />
+  // <>
+  //   <StarRating
+  //     maxRating={5}
+  //     message={["terrible", "bad", "okay", "good", "amazing"]}
+  //   />
+  //   <StarRating
+  //     maxRating={10}
+  //     size="24"
+  //     color="pink"
+  //     className="test"
+  //     defaultRating={3}
+  //   />
+  //   <Test />
+  // </>
+);
